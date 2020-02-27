f2659f606a69d536cc8286fa553d9bec4663b930
bryanm-lagash <bryanm@lagash.com>
2020-02-27 09:52:49 -0300

    Se corrige componente de lista de usuarios

--- SorteoApp/src/components/listaUsers.js
+++ SorteoApp/src/components/listaUsers.js
@@ -6,9 +6,9 @@ import Paper from '@material-ui/core/Paper';
 import * as firebase from 'firebase';
 
 import useMount from '../hooks/useMount';
-import jsonApi from '../services/jsonApi';
 import useStyles from '../containers/styles';
 import { setUserList } from '../actions/sorteo';
+import './ListaUsers.css';
 
 firebase.initializeApp(window.firebaseConfig);
 
@@ -36,20 +36,19 @@ const ListaUsers = () => {
     return (
       <React.Fragment>
         <Grid
-          style={{
-            gridTemplateColumns: '1fr 1fr 1fr',
-            display: 'Grid',
-            margin: 'auto'
-          }}
-          className={classes.List}
-          item
-          xs={6}
+          className='GridListUsers'
+          // item
+          // xs={10}
+          // sm={10}
+          // md={10}
+          // lg={10}
+          // xl={10}
         >
           {Object.values(userList).map(({ nombre }) => (
             <ListItem className={classes.List} key={nombre}>
               <ListItem alignItems='flex-start'>
                 <Paper className='paper'>
-                  <ListItemText className='icono' primary={`${nombre}`} />
+                  <ListItemText className='icono' primary={nombre} />
                 </Paper>
               </ListItem>
             </ListItem>
@@ -60,28 +59,13 @@ const ListaUsers = () => {
   }
 
   return (
-    <div className='caja container'>
-      <div className='cart2 class="row">'>
-        <Container className='raduis col-md-4'>
-          <div style={{ textAlign: 'center' }}>
-            <font className='titulo' face='Roboto' size=' 7'>
-              Lista de Usuarios
-              <p className='numeroParticipantes'>{`Numero de participantes ${userList.length}`}</p>
-            </font>
-            <Container className={classes.container} maxWidth={false}>
-              <div className={classes.root}>
-                <Grid container spacing={1}>
-                  <Grid container item xs={12} spacing={3}>
-                    <FormRow />
-                  </Grid>
-                </Grid>
-              </div>
-              <br />
-            </Container>
-          </div>
-        </Container>
+    <Container className='raduis'>
+      <div style={{ textAlign: 'center' }}>
+        <h2>Lista de Usuarios</h2>
+        <p className='numeroParticipantes'>{`Numero de participantes: ${userList.length}`}</p>
       </div>
-    </div>
+      <FormRow />
+    </Container>
   );
 };
 
